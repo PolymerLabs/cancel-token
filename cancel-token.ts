a64eca242be1bf29d4aaa7ceb4a9228f61a69458
Peter Burns <rictic@google.com>
2016-12-16 08:27:13 -0800

    Initial commit of a cancel-token polyfill

--- cancel-token.ts
+++ cancel-token.ts
@@ -15,23 +15,31 @@
 export type CancelFunction = (reason?: any) => void;
 
 /**
- * A partial polyfill for https://tc39.github.io/proposal-cancelable-promises/
+ * A polyfill for the November 23rd, 2016 draft of
+ * https://tc39.github.io/proposal-cancelable-promises/
  */
 export class CancelToken {
-  promise: Promise<void>;
-  reason: any;
+  promise: Promise<Cancel>;
+  reason: Cancel|undefined = undefined;
 
-  private _cancelled = false;
-  constructor(cb: (cancel: CancelFunction) => void) {
-    let reject: CancelFunction;
-    this.promise = new Promise<void>((_, rej) => reject = rej);
+  constructor(executor: (cancel: CancelFunction) => void) {
+    // Shouldn't this reject?
+    // Filed https://github.com/tc39/proposal-cancelable-promises/issues/69
+    // to clarify.
+    let resolve: CancelFunction;
+    this.promise = new Promise<Cancel>((res) => resolve = res);
 
     const cancel = (reason?: any) => {
-      this.reason = reason;
-      this._cancelled = true;
-      reject(new Cancel(reason));
+      if (this.reason) {
+        return;
+      }
+      // TODO(rictic): file a bug about this. This isn't up to spec, but without
+      // this every canceltoken composition system (like .race()) will add
+      // another layer of Cancel().
+      this.reason = isCancel(reason) ? reason : new Cancel(reason);
+      resolve(this.reason);
     };
-    cb(cancel);
+    executor(cancel);
   }
   static source() {
     let cancel: CancelFunction = null as any;
@@ -40,25 +48,55 @@ export class CancelToken {
     });
     return {token, cancel};
   }
-  static never = new CancelToken(() => null);
+  static race(tokens: Iterable<CancelToken>): CancelToken {
+    // TODO(rictic): This may not be right according to spec. The timing might
+    //     be off and cancellation may need to happen synchronously?
+    let cancel: CancelFunction;
+    let token = new CancelToken((c) => cancel = c);
+    for (const token of tokens) {
+      if (!isCancelToken(token)) {
+        throw new TypeError(
+            'CancelToken.race must be called with an iterable of CancelTokens');
+      }
+      token.promise.then((cancelation) => {
+        cancel(cancelation && cancelation.message || cancelation);
+      });
+    }
+
+    return token;
+  }
 
   throwIfRequested() {
-    if (this._cancelled) {
-      throw new Cancel(this.reason);
+    if (this.reason) {
+      throw this.reason;
     }
   }
 
-  // TODO(rictic): implement `static race(...cancelTokens):CancelToken`
+  get[Symbol.toStringTag]() {
+    return 'CancelToken';
+  }
+
+  // This would be useful.
+  // static never = new CancelToken(() => null);
 }
 
 export class Cancel {
-  constructor(public message?: any) {
+  message: string;
+  constructor(reason?: any) {
+    this.message = '' + reason;
+  }
+
+  toString() {
+    if (!this.message) {
+      return 'Cancel';
+    }
+    return `Cancel: ${this.message}`;
   }
 }
 
 // non-standard, taken from domenic's suggestion at
 // https://github.com/tc39/proposal-cancelable-promises/issues/32#issuecomment-235644656
-export function isCancel(value: any): boolean {
+export function isCancel(value: any): value is Cancel {
   if (!value) {
     return false;
   }
@@ -68,6 +106,18 @@ export function isCancel(value: any): boolean {
   return value.constructor.name === 'Cancel';
 }
 
+export function isCancelToken(value: any): value is CancelToken {
+  if (!value) {
+    return false;
+  }
+  if (!value.constructor) {
+    return false;
+  }
+  return value.constructor.name === 'CancelToken';
+}
+
+// TODO(rictic): handle unhandledRejections of Cancels according to each
+//     platform's folkways.
 process.on('unhandledRejection', (reason: any, p: Promise<any>) => {
   if (isCancel(reason)) {
     p.catch(() => {/*do nothing but let node know this is ok */});
